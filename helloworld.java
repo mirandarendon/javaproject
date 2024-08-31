1126f6ee8b455ec739d409eae8c10dbe737d0787
Miranda Rendon <rendonmiranda05@gmail.com>
2024-08-30 23:44:35 -0700

    remove unneccesary code

--- helloworld.java
+++ helloworld.java
@@ -5,8 +5,6 @@ public class helloworld {
     
     public static void main(String[] args) {
         
-        System.out.println("Hello World!");
-        
         Scanner scnr = new Scanner(System.in);
 
         System.out.println("What is your name?");
@@ -14,8 +12,6 @@ public class helloworld {
 
         System.out.println("How old are you?");
         int age = scnr.nextInt();
-
-        System.out.println("Thanks!");
 
         System.out.println("Hi " + name + " you are " + age + " years old.");
 
