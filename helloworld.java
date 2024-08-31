0fc7ecc12b04e722c3551b7a83ec3d8e3e3acab4
Miranda Rendon <rendonmiranda05@gmail.com>
2024-08-30 23:33:53 -0700

    Gathered input and print user name

--- helloworld.java
+++ helloworld.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
+
 public class helloworld {
     
     public static void main(String[] args) {
         
         System.out.println("Hello World!");
-        System.out.println("this is a learning curve fs");
         
+        Scanner scnr = new Scanner(System.in);
 
+        System.out.println("What is your name?");
+        String name = scnr.nextLine();
 
+        System.out.println("Hi " + name);
 
+scnr.close();
     }
 }
